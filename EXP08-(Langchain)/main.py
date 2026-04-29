5a26d433b2e3193bac8d9651cd9f9783d4794434
patilneha-05 <patilneha0518@gmail.com>
2026-04-29 15:51:44 +0530

    EXPERIMENT NO - 08

---
+++ EXP08-(Langchain)/main.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+from google import genai
+
+# Load .env
+load_dotenv()
+
+# Get API key
+api_key = os.getenv("GEMINI_API_KEY")
+
+# Create client
+client = genai.Client(api_key=api_key)
+
+# Generate content
+response = client.models.generate_content(
+    model="gemini-2.5-flash-lite",
+    contents="What is Artificial Intelligence?"
+)
+
+# Print response
+print(response.text)
